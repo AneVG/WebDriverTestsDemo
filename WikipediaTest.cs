5733424de9b9989bff4581b7f11bafdc41db16f2
HP <HP@DESKTOP-GE4JHOV>
2022-05-18 21:35:08 +0300

    Add project files.

---
+++ WikipediaTest.cs
@@ -0,0 +1,26 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Create new Chrome browser istance
+var driver = new ChromeDriver();
+
+//Navigate to Wikipedia
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE " + driver.Title);
+
+// locate search by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+//click on element
+searchField.Click();
+
+//fill QA and press ENTER keyboard button
+searchField.SendKeys("QA" + Keys.Enter);
+
+System.Console.WriteLine("TITLE AFTER SEARCH " + driver.Title);
+
+//Close Browser
+driver.Quit();
+
